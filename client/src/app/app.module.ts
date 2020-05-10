9ce34156c5e656fd5104453713cea2212a63b247
Israel <israel.araujo12@gmail.com>
2020-05-10 19:04:43 +0100

    login html

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -22,7 +29,14 @@ import { LoginComponent } from './auth/login/login.component';
         AppRoutingModule,
         BrowserAnimationsModule,
         MatToolbarModule,
-        MatIconModule
+        MatIconModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatButtonModule,
+        MatRadioModule,
+        MatCardModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [
