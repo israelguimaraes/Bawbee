8d58ad849c27c266edc3ef107e3f6c55da459045
Israel <israel.araujo12@gmail.com>
2020-05-10 15:58:39 +0100

    added user, login component and routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+    {
+        path: 'user', component: UserComponent,
+        children: [
+            { path: 'login', component: LoginComponent }
+        ]
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
