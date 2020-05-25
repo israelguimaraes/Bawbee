42ff98d5558c193301957559b5ae4b9a05da303b
= <Israel.arauj0@live.com>
2020-05-26 00:25:09 +0100

    Command, Message, Mediatr, FluentValidation

---
+++ server/Bawbee.Domain.Core/Commands/Command.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bawbee.Domain.Core.Commands
+{
+    public abstract class Command<TAggregateId> : Message<TAggregateId>
+    {
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ server/Bawbee.Domain.Core/Commands/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace Bawbee.Domain.Core.Commands
+{
+    public abstract class Message<T>
+    {
+        public string MessageType { get; protected set; }
+        public T AggregateId { get; protected set; }
+
+        public Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
