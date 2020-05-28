8401aabb414438ce7a17e336b95fc211ecb1576b
= <Israel.arauj0@live.com>
2020-05-28 19:25:13 +0100

    DbContext

--- server/Bawbee.Infra.Data/EntityFramework/Contexts/BawbeeDbContext.cs
+++ server/Bawbee.Infra.Data/EntityFramework/Contexts/BawbeeDbContext.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Bawbee.Domain.Entities;
+using Bawbee.Infra.Data.EntityFramework.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Bawbee.Infra.Data.EntityFramework.Contexts
 {
-    class BawbeeDbContext
+    public class BawbeeDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<EntryCategory> EntryCategories { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<UserContext> UserContexts { get; set; }
+
+        public BawbeeDbContext(DbContextOptions<BawbeeDbContext> options)
+            : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new UserMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ server/Bawbee.Infra.Data/EntityFramework/Mappings/UserMapping.cs
@@ -0,0 +1,18 @@
+﻿using Bawbee.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bawbee.Infra.Data.EntityFramework.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+        }
+    }
+}
