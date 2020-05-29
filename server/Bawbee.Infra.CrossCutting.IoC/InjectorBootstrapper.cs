f147c6f19bb373fc65d00a88e13a90e7b783ad0b
= <Israel.arauj0@live.com>
2020-05-29 20:00:55 +0100

    IoC: first dependencies

--- server/Bawbee.Infra.CrossCutting.IoC/InjectorBootstrapper.cs
+++ server/Bawbee.Infra.CrossCutting.IoC/InjectorBootstrapper.cs
@@ -1,5 +1,15 @@
-﻿using Bawbee.Domain.Core.Bus;
+﻿using Bawbee.Application.Interfaces;
+using Bawbee.Application.Services;
+using Bawbee.Domain.CommandHandlers;
+using Bawbee.Domain.Commands.Users;
+using Bawbee.Domain.Core.Bus;
+using Bawbee.Domain.Core.Notifications;
+using Bawbee.Domain.EventHandlers;
+using Bawbee.Domain.Events.Users;
+using Bawbee.Domain.Interfaces;
 using Bawbee.Infra.CrossCutting.Bus;
+using Bawbee.Infra.Data.EntityFramework.Repositories;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bawbee.Infra.CrossCutting.IoC
@@ -12,18 +22,18 @@ namespace Bawbee.Infra.CrossCutting.IoC
             services.AddScoped<IMediatorHandler, InMemoryBus>();
 
             // Domain - Events
-            //services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
-            //services.AddScoped<INotificationHandler<UserRegisteredEvent>, UserEventHandler>();
-            //services.AddScoped<INotificationHandler<UserRegisteredEvent>, EmailEventHandler>();
+            services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            services.AddScoped<INotificationHandler<UserRegisteredEvent>, UserEventHandler>();
+            services.AddScoped<INotificationHandler<UserRegisteredEvent>, EmailEventHandler>();
 
-            //// Domain - Commands
-            //services.AddScoped<IRequestHandler<RegisterNewUserCommand, bool>, UserCommandHandler>();
+            // Domain - Commands
+            services.AddScoped<IRequestHandler<RegisterNewUserCommand, bool>, UserCommandHandler>();
 
-            //// Infra.Data
-            //services.AddScoped<UserRepository, IUserRepository>();
+            // Infra.Data
+            services.AddScoped<IUserRepository, UserRepository>();
 
-            //// Application
-            //services.AddScoped<IUserApplication, UserApplication>();
+            // Application
+            services.AddScoped<IUserApplication, UserApplication>();
         }
     }
 }
