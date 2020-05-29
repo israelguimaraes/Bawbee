c1f987c92ad4cb48cae2638e1742947248d6c507
= <Israel.arauj0@live.com>
2020-05-29 19:57:08 +0100

    EmailEventHandler, UserEventHandler

---
+++ server/Bawbee.Domain/EventHandlers/EmailEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Bawbee.Domain.Events.Users;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bawbee.Domain.EventHandlers
+{
+    public class EmailEventHandler
+        : INotificationHandler<UserRegisteredEvent>
+    {
+        public Task Handle(UserRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO: send e-mail
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ server/Bawbee.Domain/EventHandlers/UserEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Bawbee.Domain.Events.Users;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bawbee.Domain.EventHandlers
+{
+    public class UserEventHandler 
+        : INotificationHandler<UserRegisteredEvent>
+    {
+        public Task Handle(UserRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            // TODO: send e-mail (validation user)
+            return Task.CompletedTask;
+        }
+    }
+}
