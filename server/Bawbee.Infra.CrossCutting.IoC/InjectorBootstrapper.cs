a419db20115d20fb3e5977f8622f400dfcbb2c7e
= <Israel.arauj0@live.com>
2020-05-29 19:58:18 +0100

    .

--- server/Bawbee.Infra.CrossCutting.IoC/InjectorBootstrapper.cs
+++ server/Bawbee.Infra.CrossCutting.IoC/InjectorBootstrapper.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using Bawbee.Domain.Core.Bus;
+using Bawbee.Infra.CrossCutting.Bus;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bawbee.Infra.CrossCutting.IoC
 {
     public class InjectorBootstrapper
     {
+        public void RegisterDependencies(IServiceCollection services)
+        {
+            // Domain
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            // Domain - Events
+            //services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            //services.AddScoped<INotificationHandler<UserRegisteredEvent>, UserEventHandler>();
+            //services.AddScoped<INotificationHandler<UserRegisteredEvent>, EmailEventHandler>();
+
+            //// Domain - Commands
+            //services.AddScoped<IRequestHandler<RegisterNewUserCommand, bool>, UserCommandHandler>();
+
+            //// Infra.Data
+            //services.AddScoped<UserRepository, IUserRepository>();
+
+            //// Application
+            //services.AddScoped<IUserApplication, UserApplication>();
+        }
     }
 }
