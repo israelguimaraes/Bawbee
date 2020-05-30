cced373ee8e698170a12c465d0050e171e89784f
= <Israel.arauj0@live.com>
2020-05-30 15:47:17 +0100

    .

---
+++ server/Bawbee.Domain.Queries/Users/DTOs/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bawbee.Domain.Queries.Users.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ server/Bawbee.Domain.Queries/Users/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,11 @@
+﻿using Bawbee.Domain.Core.Commands;
+using Bawbee.Domain.Queries.Users.DTOs;
+using System.Collections.Generic;
+
+namespace Bawbee.Domain.Queries.Users.Queries
+{
+    public class GetAllUsersQuery : CommandQuery<IEnumerable<UserDTO>>
+    {
+
+    }
+}
