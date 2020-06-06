8754b5cca793ea2bf9f22574f1d794cd20cb1b68
= <Israel.arauj0@live.com>
2020-06-06 16:48:42 +0100

    Fluent Validation ok

---
+++ server/Bawbee.Domain.Commands/Users/Validators/RegisterNewUserValidator.cs
@@ -0,0 +1,34 @@
+﻿using Bawbee.Domain.Commands.Users.Commands;
+using FluentValidation;
+
+namespace Bawbee.Domain.Commands.Users.Validators
+{
+    public class RegisterNewUserValidator : AbstractValidator<RegisterNewUserCommand>
+    {
+        public const int PASSWORD_MIN_LENGTH = 6;
+        public const int PASSWORD_MAX_LENGTH = 10;
+
+        public RegisterNewUserValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(c => c.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required");
+
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .WithMessage("E-mail is required")
+                .EmailAddress()
+                .WithMessage("E-mail is invalid");
+
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .Length(6, 10)
+                .WithMessage($"Password must be between {PASSWORD_MIN_LENGTH} and {PASSWORD_MAX_LENGTH} characters");
+        }
+    }
+}
