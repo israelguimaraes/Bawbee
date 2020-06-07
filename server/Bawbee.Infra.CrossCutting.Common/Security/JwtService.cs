d9057d5e42745b390abab126555966d193514c0c
= <Israel.arauj0@live.com>
2020-06-07 12:02:29 +0100

    appsettings.json, UserTokenDTO and JwtService class

---
+++ server/Bawbee.Infra.CrossCutting.Common/Security/JwtService.cs
@@ -0,0 +1,47 @@
+﻿using Bawbee.Infra.CrossCutting.Common.Security.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Bawbee.Infra.CrossCutting.Common.Security
+{
+    public class JwtService
+    {
+        private readonly string _secret;
+        private readonly string _expDate;
+
+        public JwtService(IConfiguration configuration)
+        {
+            var jwtConfig = configuration.GetSection("JwtConfig");
+
+            _secret = jwtConfig.GetSection("secret").Value;
+            _expDate = jwtConfig.GetSection("expirationHours").Value;
+        }
+
+        public string GenerateSecurityToken(UserTokenDTO user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secret);
+            
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Name),
+                }),
+                Expires = DateTime.UtcNow.AddHours(double.Parse(_expDate)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var result = tokenHandler.WriteToken(token);
+
+            return result;
+        }
+    }
+}
