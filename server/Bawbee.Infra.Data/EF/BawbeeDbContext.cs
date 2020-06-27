35e1ec096dc860323e9837790ee6c61047fb19e6
= <Israel.arauj0@live.com>
2020-06-27 21:22:20 +0100

    EF package and BawbeeDbContext added

---
+++ server/Bawbee.Infra.Data/EF/BawbeeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Bawbee.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bawbee.Infra.Data.EF
+{
+    public class BawbeeDbContext : DbContext
+    {
+        public BawbeeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<EntryCategory> EntryCategories { get; set; }
+    }
+}
