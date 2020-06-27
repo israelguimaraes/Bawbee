216e4bc25bfe01721e1745dcda11ef5c32eceec0
= <Israel.arauj0@live.com>
2020-06-27 21:16:24 +0100

    EntryCategory - method GetDefaultCategoriesForNewUsers

--- server/Bawbee.Domain/Entities/EntryCategory.cs
+++ server/Bawbee.Domain/Entities/EntryCategory.cs
@@ -1,4 +1,5 @@
 ﻿using Bawbee.Domain.Core.Models;
+using System.Collections.Generic;
 
 namespace Bawbee.Domain.Entities
 {
@@ -7,12 +8,29 @@ namespace Bawbee.Domain.Entities
         public int EntryCategoryId { get; private set; }
         public string Name { get; private set; }
         public int UserId { get; private set; }
-        public User User { get; private set; }
 
         public EntryCategory(string name, int userId)
         {
             Name = name;
             UserId = userId;
+        }
+
+        public static IEnumerable<EntryCategory> GetDefaultCategoriesForNewUsers(int userId)
+        {
+            var list = new List<EntryCategory>
+            {
+                new EntryCategory("House", userId),
+                new EntryCategory("Food", userId),
+                new EntryCategory("Grocery", userId),
+                new EntryCategory("Leisure and Fun", userId),
+                new EntryCategory("Transport", userId),
+                new EntryCategory("Debts and Loans", userId),
+                new EntryCategory("Shoppings", userId),
+                new EntryCategory("Salary", userId),
+                new EntryCategory("Investments", userId)
+            };
+
+            return list;
         }
     }
 }
