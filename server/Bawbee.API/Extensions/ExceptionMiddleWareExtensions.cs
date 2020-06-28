66af42a58e64de0920285b44733f61006da4b3c6
= <Israel.arauj0@live.com>
2020-06-28 17:19:17 +0100

    ExceptionMiddlewareExteneions added

---
+++ server/Bawbee.API/Extensions/ExceptionMiddleWareExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Bawbee.API.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace Bawbee.API.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        // TODO log
+                        //logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        var errorDetail = new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error."
+                        };
+
+                        await context.Response.WriteAsync(errorDetail.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
