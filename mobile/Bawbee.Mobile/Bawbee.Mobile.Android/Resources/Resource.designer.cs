e56ed3c075ea330b81636f4a3506467c01661352
= <Israel.arauj0@live.com>
2020-07-05 22:28:12 +0100

    mobile: login page wallet image

--- mobile/Bawbee.Mobile/Bawbee.Mobile.Android/Resources/Resource.designer.cs
+++ mobile/Bawbee.Mobile/Bawbee.Mobile.Android/Resources/Resource.designer.cs
@@ -8357,7 +8357,10 @@ namespace Bawbee.Mobile.Droid
 			public const int tooltip_frame_light = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int xamarin_logo = 2131165302;
+			public const int wallet = 2131165302;
+			
+			// aapt resource value: 0x7F070077
+			public const int xamarin_logo = 2131165303;
 			
 			static Drawable()
 			{
