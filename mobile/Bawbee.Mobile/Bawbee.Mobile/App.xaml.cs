de5f6009adcf1abe876f7da0edcaf7f88bb34db3
= <Israel.arauj0@live.com>
2020-07-05 13:17:36 +0100

    Xamarin project

---
+++ mobile/Bawbee.Mobile/Bawbee.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Bawbee.Mobile.Services;
+using Bawbee.Mobile.Views;
+
+namespace Bawbee.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
