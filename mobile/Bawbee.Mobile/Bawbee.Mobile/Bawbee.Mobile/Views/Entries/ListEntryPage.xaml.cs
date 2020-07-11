fe73eaedfa92d06d161784da8ba707aee2a67202
= <Israel.arauj0@live.com>
2020-07-11 21:09:48 +0100

    list entry - on apppearing and disappearing

--- mobile/Bawbee.Mobile/Bawbee.Mobile/Bawbee.Mobile/Views/Entries/ListEntryPage.xaml.cs
+++ mobile/Bawbee.Mobile/Bawbee.Mobile/Bawbee.Mobile/Views/Entries/ListEntryPage.xaml.cs
@@ -1,7 +1,5 @@
-﻿using Bawbee.Mobile.ReadModels.Entries;
-using System;
-using System.Collections.Generic;
-
+﻿
+using Bawbee.Mobile.ViewModels.Entries;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,42 +8,28 @@ namespace Bawbee.Mobile.Views.Entries
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListEntryPage : ContentPage
     {
-        public ICollection<EntryReadModel> Entries { get; set; }
-
         public ListEntryPage()
         {
             InitializeComponent();
 
-            Entries = new List<EntryReadModel>();
-
-            GenerateFakeData();
-
-            BindingContext = this;
+            BindingContext = new ListEntryViewModel();
         }
 
-        private void GenerateFakeData()
+        protected override async void OnAppearing()
         {
-            for (int i = 1; i <= 5; i++)
-            {
-                var entry = new EntryReadModel
-                {
-                    Id = i,
-                    Description = $"lorem ipsum {i}",
-                    Value = (i + 2) * 8.33m,
-                    //Value = 123456789.98m,
-                    CategoryName = $"category {i}",
-                    BankAccountName = "ActivoBank",
-                    IsPaid = i % 2 == 0,
-                    CreatedAt = DateTime.Now.AddDays(i - 1)
-                };
+            base.OnAppearing();
 
-                if (i == 30)
-                {
-                    entry.Value = 1_000_000.89m;
-                }
-                
-                Entries.Add(entry);
-            }
+            MessagingCenter.Subscribe<ListEntryViewModel>(this, nameof(ListEntryViewModel.OpenModalNewEntryCommand), async (msg) =>
+            {
+                await Navigation.PushAsync(new AddEntryPage());
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            MessagingCenter.Unsubscribe<ListEntryViewModel>(this, nameof(ListEntryViewModel.OpenModalNewEntryCommand));
         }
     }
 }
