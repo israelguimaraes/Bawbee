1dac556cffdfce31d7fd546d16c631b7f1e2c4d6
= <Israel.arauj0@live.com>
2020-07-21 15:56:10 +0100

    fix

--- server/Bawbee.Application.Query/Users/Handlers/EntryQueryHandler.cs
+++ server/Bawbee.Application.Query/Users/Handlers/EntryQueryHandler.cs
@@ -4,6 +4,7 @@ using Bawbee.Application.Query.Users.Queries.Entries;
 using Bawbee.Application.Query.Users.ReadModels;
 using Bawbee.Application.Query.Users.ReadModels.Entries;
 using Bawbee.Domain.Core.Commands;
+using Bawbee.Infra.CrossCutting.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -42,20 +43,19 @@ namespace Bawbee.Application.Query.Users.Handlers
         {
             var expenses = await _entryReadRepository.GetAllExpensesByMonth(query.UserId, query.Month);
 
-            var groupedByCategory = expenses.GroupBy(e => e.EntryCategoryId).ToList();
+            var groupedByCategory = expenses.GroupBy(e => e.EntryCategoryId);
 
             var result = new List<MonthExpenseReadModel>();
-            var totalExpenses = expenses.Sum(e => e.Value) * -1;  // TODO: * -1 => add extension
+            var totalExpenses = expenses.Sum(e => e.Value).ToPositive();
 
             foreach (var expensesByCategory in groupedByCategory)
             {
                 var readModel = new MonthExpenseReadModel();
                 readModel.Category = expenses.FirstOrDefault(e => e.EntryCategoryId == expensesByCategory.Key).EntryCategoryName;
                 
-                // TODO: * -1 => add extension
-                readModel.TotalValue = expensesByCategory.Sum(e => e.Value) * -1;
-                
-                readModel.Percent = readModel.TotalValue / totalExpenses * 100;
+                readModel.TotalValue = expensesByCategory.Sum(e => e.Value).ToPositive();
+
+                readModel.Percent = (readModel.TotalValue / totalExpenses * 100).To2DecimalPlaces();
 
                 result.Add(readModel);
             }
