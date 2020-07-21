c3ea06aba4cf24c449531053f1695f7c169aaf05
= <Israel.arauj0@live.com>
2020-07-21 11:47:38 +0100

    .

---
+++ mobile/Bawbee.Mobile/Bawbee.Mobile/Bawbee.Mobile/Models/Dashboards/Dashboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Bawbee.Mobile.Models.Dashboards
+{
+    public class Dashboard
+    {
+        public string UserName { get; set; }
+        public decimal TotalCurrentBalance { get; set; }
+
+        public IEnumerable<BankAccount> BankAccounts { get; set; }
+        public IEnumerable<MonthExpense> CurrentMonthExpenses { get; set; }
+    }
+}
