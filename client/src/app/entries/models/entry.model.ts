f06f30a203c1a45a2c2ff62ff9aad33f8395c72b
= <Israel.arauj0@live.com>
2020-09-16 15:58:00 -0300

    removed client project

--- client/src/app/entries/models/entry.model.ts
+++
@@ -1,9 +0,0 @@
-export class Entry {
-    id: number;
-    description: string;
-    value: number;
-    date: string;
-    isFinished: boolean;
-    bankAccountName: string;
-    categoryName: string;
-}
